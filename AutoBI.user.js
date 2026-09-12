0ec3d9af10696a00544c99f17acef00340356871
PhamngocNDH <129825999+PhamngocNDH@users.noreply.github.com>
2026-09-12 17:02:43 +0700

    Release AutoBI 16.1.1.41
    Populate Sức khỏe ST installment figures directly from Báo cáo trả chậm, keep filter/table waiting reliable, update the stable installer, and remove version 16.1.1.40 from the current tree.

--- AutoBI.user.js
+++ AutoBI.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AutoBI
 // @namespace    https://github.com/PhamngocNDH/AutoBI
-// @version      16.1.1.40
-// @description  AutoBI 16.1.1.40 - chờ bảng Nhân viên ổn định trước khi loại Online QĐ
+// @version      16.1.1.41
+// @description  AutoBI 16.1.1.41 - lấy Trả chậm trực tiếp từ Báo cáo trả chậm
 // @author       38967 - Mr Phạm
 // @match        https://crm.thegioididong.com/*
 // @match        https://baocao.dienmayxanh.com/*
@@ -16,7 +16,7 @@
 // @grant        GM_xmlhttpRequest
 // @require      https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
-// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.40.user.js
+// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.41.user.js
 // @updateURL    https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // @downloadURL  https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // ==/UserScript==
