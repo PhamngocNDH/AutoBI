04257912180d3ff98f067066548faec7996b3a45
PhamngocNDH <129825999+PhamngocNDH@users.noreply.github.com>
2026-09-12 09:23:07 +0700

    Release AutoBI 16.1.1.39 to test automatic updates
    Bump version labels and the stable loader require URL only. Preserve the tested 16.1.1.38 behavior.

--- AutoBI.user.js
+++ AutoBI.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AutoBI
 // @namespace    https://github.com/PhamngocNDH/AutoBI
-// @version      16.1.1.38
-// @description  AutoBI 16.1.1.38 - loại doanh thu QĐ Online khỏi target siêu thị
+// @version      16.1.1.39
+// @description  AutoBI 16.1.1.39 - loại doanh thu QĐ Online khỏi target siêu thị
 // @author       38967 - Mr Phạm
 // @match        https://crm.thegioididong.com/*
 // @match        https://baocao.dienmayxanh.com/*
@@ -16,7 +16,7 @@
 // @grant        GM_xmlhttpRequest
 // @require      https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
-// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.38.user.js
+// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.39.user.js
 // @updateURL    https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // @downloadURL  https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // ==/UserScript==
