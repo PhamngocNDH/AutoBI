278eed08300a7f86fd6f464d5742e8b7abb9cf08
PhamngocNDH <129825999+PhamngocNDH@users.noreply.github.com>
2026-09-12 09:11:48 +0700

    Release AutoBI 16.1.1.38
    Exclude Online 18001060 converted revenue from each shop's cumulative target and forecast on every scan. Publish the user-tested build and update the stable loader to 16.1.1.38.

--- AutoBI.user.js
+++ AutoBI.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AutoBI
 // @namespace    https://github.com/PhamngocNDH/AutoBI
-// @version      16.1.1.35
-// @description  AutoBI 16.1.1.35 CLEAN - stable
+// @version      16.1.1.38
+// @description  AutoBI 16.1.1.38 - loại doanh thu QĐ Online khỏi target siêu thị
 // @author       38967 - Mr Phạm
 // @match        https://crm.thegioididong.com/*
 // @match        https://baocao.dienmayxanh.com/*
@@ -16,7 +16,7 @@
 // @grant        GM_xmlhttpRequest
 // @require      https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
-// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.16.user.js
+// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.38.user.js
 // @updateURL    https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // @downloadURL  https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // ==/UserScript==
