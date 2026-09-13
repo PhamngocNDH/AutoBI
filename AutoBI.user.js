6c6039f86cbaf9979b1f6111651db2c3d9e607c6
PhamngocNDH <129825999+PhamngocNDH@users.noreply.github.com>
2026-09-13 08:47:45 +0700

    Point AutoBI loader to 16.1.1.44

--- AutoBI.user.js
+++ AutoBI.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AutoBI
 // @namespace    https://github.com/PhamngocNDH/AutoBI
-// @version      16.1.1.43
-// @description  AutoBI 16.1.1.43 - thêm chế độ Doanh Thu & Thi Đua Realtime nhanh
+// @version      16.1.1.44
+// @description  AutoBI 16.1.1.44 - sửa dự kiến khi xem lại dữ liệu đã lưu
 // @author       38967 - Mr Phạm
 // @match        https://crm.thegioididong.com/*
 // @match        https://baocao.dienmayxanh.com/*
@@ -16,7 +16,7 @@
 // @grant        GM_xmlhttpRequest
 // @require      https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
-// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.43.user.js
+// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.44.user.js
 // @updateURL    https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // @downloadURL  https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // ==/UserScript==
