9c3cf0059343ef3f4072e4f702eba5aadaa432fe
PhamngocNDH <129825999+PhamngocNDH@users.noreply.github.com>
2026-09-15 17:15:28 +0700

    Release AutoBI 16.1.1.53 stable

--- AutoBI.user.js
+++ AutoBI.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AutoBI
 // @namespace    https://github.com/PhamngocNDH/AutoBI
-// @version      16.1.1.52
-// @description  AutoBI 16.1.1.52 STABLE - sửa Sức khỏe ST từng siêu thị
+// @version      16.1.1.53
+// @description  AutoBI 16.1.1.53 STABLE - kiểm soát đầy đủ dữ liệu Realtime, lũy kế và Sức khỏe ST
 // @author       38967 - Mr Phạm
 // @match        https://crm.thegioididong.com/*
 // @match        https://baocao.dienmayxanh.com/*
@@ -18,6 +18,7 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
 // @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.44.user.js
 // @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_health_shop_fix.user.js
+// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_data_integrity.user.js
 // @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_version_badge.user.js
 // @updateURL    https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // @downloadURL  https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
