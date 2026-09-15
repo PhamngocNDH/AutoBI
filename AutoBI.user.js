ecc39ad559e274ff54f1c627e120e2b0c9a8049c
PhamngocNDH <129825999+PhamngocNDH@users.noreply.github.com>
2026-09-15 10:33:07 +0700

    Restore AutoBI 16.1.1.44 as stable release

--- AutoBI.user.js
+++ AutoBI.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AutoBI
 // @namespace    https://github.com/PhamngocNDH/AutoBI
-// @version      16.1.1.49
-// @description  AutoBI 16.1.1.49 - khóa Báo cáo Phục vụ trong khung 08:00-17:59
+// @version      16.1.1.44
+// @description  AutoBI 16.1.1.44 - bản ổn định
 // @author       38967 - Mr Phạm
 // @match        https://crm.thegioididong.com/*
 // @match        https://baocao.dienmayxanh.com/*
@@ -16,7 +16,7 @@
 // @grant        GM_xmlhttpRequest
 // @require      https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
-// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.49.user.js
+// @require      https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI_16.1.1.44.user.js
 // @updateURL    https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // @downloadURL  https://raw.githubusercontent.com/PhamngocNDH/AutoBI/main/AutoBI.user.js
 // ==/UserScript==
